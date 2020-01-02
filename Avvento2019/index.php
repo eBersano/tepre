2b604e96f7091906f08b60e4e37d4143a3f41a6d
Edoardo Bersano <41066798+eBersano@users.noreply.github.com>
2020-01-02 19:34:42 +0100

    Create index.php

---
+++ Avvento2019/index.php
@@ -0,0 +1,675 @@
+<HTML>
+<html lang="it">
+<head>
+
+  <title>Il Calendario dell'Avvento di Edoardo Bersano</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta name="Description" CONTENT="Il Calendario dell'Avvento di Edoardo Bersano">
+   
+   
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  
+  <style>
+ img {
+max-width: 100%;
+}
+
+/* customizable snowflake styling */
+.snowflake {
+  color: #fff;
+  font-size: 1em;
+  font-family: Arial, sans-serif;
+  text-shadow: 0 0 5px #000;
+}
+
+@-webkit-keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@-webkit-keyframes snowflakes-shake{0%,100%{-webkit-transform:translateX(0);transform:translateX(0)}50%{-webkit-transform:translateX(80px);transform:translateX(80px)}}@keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@keyframes snowflakes-shake{0%,100%{transform:translateX(0)}50%{transform:translateX(80px)}}.snowflake{position:fixed;top:-10%;z-index:9999;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;cursor:default;-webkit-animation-name:snowflakes-fall,snowflakes-shake;-webkit-animation-duration:10s,3s;-webkit-animation-timing-function:linear,ease-in-out;-webkit-animation-iteration-count:infinite,infinite;-webkit-animation-play-state:running,running;animation-name:snowflakes-fall,snowflakes-shake;animation-duration:10s,3s;animation-timing-function:linear,ease-in-out;animation-iteration-count:infinite,infinite;animation-play-state:running,running}.snowflake:nth-of-type(0){left:1%;-webkit-animation-delay:0s,0s;animation-delay:0s,0s}.snowflake:nth-of-type(1){left:10%;-webkit-animation-delay:1s,1s;animation-delay:1s,1s}.snowflake:nth-of-type(2){left:20%;-webkit-animation-delay:6s,.5s;animation-delay:6s,.5s}.snowflake:nth-of-type(3){left:30%;-webkit-animation-delay:4s,2s;animation-delay:4s,2s}.snowflake:nth-of-type(4){left:40%;-webkit-animation-delay:2s,2s;animation-delay:2s,2s}.snowflake:nth-of-type(5){left:50%;-webkit-animation-delay:8s,3s;animation-delay:8s,3s}.snowflake:nth-of-type(6){left:60%;-webkit-animation-delay:6s,2s;animation-delay:6s,2s}.snowflake:nth-of-type(7){left:70%;-webkit-animation-delay:2.5s,1s;animation-delay:2.5s,1s}.snowflake:nth-of-type(8){left:80%;-webkit-animation-delay:1s,0s;animation-delay:1s,0s}.snowflake:nth-of-type(9){left:90%;-webkit-animation-delay:3s,1.5s;animation-delay:3s,1.5s}.snowflake:nth-of-type(10){left:25%;-webkit-animation-delay:2s,0s;animation-delay:2s,0s}.snowflake:nth-of-type(11){left:65%;-webkit-animation-delay:4s,2.5s;animation-delay:4s,2.5s}
+
+
+
+.perspective
+{
+
+            background-repeat: no-repeat;
+            background-position: center center;
+            position: relative;
+            display: inline;
+            float: left;
+            height: 224px;
+            width:  224px;
+
+            -webkit-perspective: 450;
+            border-radius: 3px;
+            box-sizing: border-box;
+        }
+        
+        .thumb
+        {
+            background: url("img/13.jpg");
+            background-repeat: no-repeat;
+            background-position: center center;
+            width:  224px;
+            height: 224px;
+            position: absolute;
+            box-sizing: border-box;
+            border-radius: 3px;
+            box-shadow: 0 0 0 10px rgba(255, 255, 255, .0) inset;
+            transition: 1s transform linear;
+            transform-origin: left;
+            cursor: pointer;
+        }
+        
+        .thumbOpened
+        {
+            transform: rotateY(-90deg);
+            transform-origin: 8px;
+            transition: .5s linear;
+        }
+
+
+@keyframes go {
+  100%{
+    opacity:1;
+    transform:rotateY(0deg);
+  }
+}
+
+
+.btn-toolset {
+ display: inline-block;
+ text-align: center;
+ white-space: nowrap;
+ color: #FFFFFF;
+ text-decoration: none;
+ line-height: 1.2;
+ font-weight: normal;
+ font-family: sans-serif;
+ font-size: 16px;
+ background-color: #006FE6;
+ border-radius: 0px;
+ padding: 10px 23px;
+ border: 2px solid #006FE6;
+ cursor: pointer;
+}
+.btn-toolset:hover {
+ background-color: #0063CC;
+ border-color: #0056B3;
+}
+.btn-toolset > strong {
+ font-size: 24px;
+}
+</style>
+<script>
+function openDoor(field) {
+            var y = $(field).find(".thumb");
+            var x = y.attr("class");
+			y.addClass("thumbOpened");
+			
+}
+
+      
+</script>
+  </head>
+   
+  <body>
+ <div class="snowflakes" aria-hidden="true">
+
+ 
+   <div class="snowflake">
+    ❆
+  </div>
+    <div class="snowflake">
+    ❆
+  </div>
+   <div class="snowflake">
+    ❆
+  </div>
+   <div class="snowflake">
+    ❆
+  </div>
+  <div class="snowflake">
+    ❆
+  </div>
+   <div class="snowflake">
+    ❆
+  </div>
+   <div class="snowflake">
+    ❆
+  </div>
+   <div class="snowflake">
+    ❆
+  </div> <div class="snowflake">
+    ❆
+  </div>
+
+</div> 
+  
+  
+  
+  
+  <div class="jumbotron jumbotron-fluid text-center" style="margin-bottom:0; background-image: url('img/header.jpg'); color: white;">
+  <div class="container">
+    <b><h1 class="display-4">Il Calendario dell'Avvento</h1></b>
+    <p class="lead"><i>by Sandy Tepre</i></p>
+  </div>
+</div>
+
+<div class="container-fluid" style="margin-top:30px">
+
+<!-- Menù sinistra -->
+  <div class="row">
+    <div class="col-sm-3" style="text-align: center;>
+	<h4 class="display-9">Men&ugrave principale</h4>
+	
+	
+	<ul class="nav nav-pills flex-column">
+	  <li class='nav-item'><a class='nav-link active' style="background-color: #165B33; color: white;">HOME</a></li>
+	  <li class='nav-item'><a class='nav-link' href='http://tepre.it' style="color: #165B33"> ❤️ Edoardo Bersano</a></li>
+	  </ul><br>
+
+
+	  
+	  
+	  
+	<br>
+<p>Immagini di copertina sotto copyright: </p>
+	<a href="http://carolinealfreds.com/"><img src="img/logo.jpg" ></a><br>
+<br><br>
+<a href="tutorial.html" class="btn-toolset">
+<strong>Tutorial Qui</strong><br>Come creare un calendario dell'Avvento
+</a>
+ 
+	</div>
+    <div class="col-sm-9" style="text-align: center; ">
+	
+	
+	  <div class="row">
+ <?php
+ $oggi = idate(d);
+?>
+<?
+ 
+ //12
+ if($oggi >= 12) {
+ 
+?>
+    <div class="col-sm-3">
+	        <div style=" background: url('https://via.placeholder.com/224');" class="perspective" onclick="openDoor(this)">
+            <div style=" background: url('img/12.jpg');"class="thumb" style="">
+            </div>
+        </div>
+
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/12.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+
+
+<?
+ ////////////////////////////////////////////////////////////////////////////////////////// 8
+ if($oggi >= 8) {
+ 
+?>
+
+    <div class="col-sm-3" style="">
+	<a href="http://tepre.it/news.php?id=23"><div style=" background: url('img/8a.jpg');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/8.jpg');"class="thumb" style=""></div></div></a>
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/8.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+
+
+
+<?
+ ////////////////////////////////////////////////////////////////////////////////////////// 23
+ if($oggi >= 23) {
+ 
+?>
+ <div class="col-sm-3" style="">
+	<div style=" background: url('https://via.placeholder.com/224');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/23.jpg');"class="thumb" style=""></div></div>
+</div>	
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/23.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+
+
+<?
+ ////////////////////////////////////////////////////////////////////////////////////////// 20
+ if($oggi >= 20) {
+ 
+?>
+	    <div class="col-sm-3" style="">
+	<div style=" background: url('https://via.placeholder.com/224');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/20.jpg');"class="thumb" style=""></div></div>
+</div>
+
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/20.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+</div><div class="row">
+
+<?
+ ////////////////////////////////////////////////////////////////////////////////////////// 9
+ if($oggi >= 9) {
+ 
+?>
+    <div class="col-sm-3" style="">
+	<a href="http://tepre.it/news.php?id=24"><div style=" background: url('img/9a.jpg');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/9.jpg');"class="thumb" style=""></div></div></a>
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/9.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+
+<?
+ ////////////////////////////////////////////////////////////////////////////////////////// 17
+ if($oggi >= 17) {
+ 
+?>
+    <div class="col-sm-3" style="">
+	<div style=" background: url('https://via.placeholder.com/224');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/17.jpg');"class="thumb" style=""></div></div>
+</div>	
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/17.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+
+<?
+ ////////////////////////////////////////////////////////////////////////////////////////// 11
+ if($oggi >= 11) {
+ 
+?>
+	    <div class="col-sm-3" style="">
+	<div style=" background: url('https://via.placeholder.com/224');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/11.jpg');"class="thumb" style=""></div></div>
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/11.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+<?
+ ////////////////////////////////////////////////////////////////////////////////////////// 3
+ if($oggi >= 3) {
+ 
+?>
+
+	    <div class="col-sm-3" style="">
+	<a href="http://tepre.it/news.php?id=18"><div style=" background: url('img/3a.jpg');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/3.jpg');"class="thumb" style=""></div></div>
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/3.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+</div>
+	  <div class="row">
+	<?
+ ////////////////////////////////////////////////////////////////////////////////////////// 22
+ if($oggi >= 22) {
+ 
+?>  
+	  
+    <div class="col-sm-3" style="">
+	<div style=" background: url('https://via.placeholder.com/224');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/22.jpg');"class="thumb" style=""></div></div>
+</div>
+
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/22.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+	<?
+ ////////////////////////////////////////////////////////////////////////////////////////// 10
+ if($oggi >= 10) {
+ 
+?>
+    <div class="col-sm-3" style="">
+	<a href="http://tepre.it/news.php?id=25"><div style=" background: url('img/10a.jpg');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/10.jpg');"class="thumb" style=""></div></div></a>
+</div>	
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/10.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+
+	<?
+ ////////////////////////////////////////////////////////////////////////////////////////// 7
+ if($oggi >= 7) {
+ 
+?> <div class="col-sm-3" style="">
+	<a><div style=" background: url('img/7a.jpg');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/7.jpg');"class="thumb" style=""></div></div></a>
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/7.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+
+	<?
+ ////////////////////////////////////////////////////////////////////////////////////////// 15
+ if($oggi >= 15) {
+ 
+?><div class="col-sm-3" style="">
+	<div style=" background: url('https://via.placeholder.com/224');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/15.jpg');"class="thumb" style=""></div></div>
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/15.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+
+
+</div>
+	  <div class="row">
+	  <?
+ ////////////////////////////////////////////////////////////////////////////////////////// 1
+ if($oggi >= 1) {
+ 
+?>
+	  
+    <div class="col-sm-3" style="">
+	<a href="https://tepre.it/news.php?id=16"><div style=" background: url('img/1a.jpg');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/1.jpg');"class="thumb" style=""></div></div></a>
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/1.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+	  <?
+ ////////////////////////////////////////////////////////////////////////////////////////// 16
+ if($oggi >= 16) {
+ 
+?>
+    <div class="col-sm-3" style="">
+	<div style=" background: url('https://via.placeholder.com/224');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/16.jpg');"class="thumb" style=""></div></div>
+</div>	
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/16.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+	  <?
+ ////////////////////////////////////////////////////////////////////////////////////////// 14
+ if($oggi >= 14) {
+ 
+?>
+	    <div class="col-sm-3" style="">
+	<div style=" background: url('https://via.placeholder.com/224');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/14.jpg');"class="thumb" style=""></div></div>
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/14.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+	  <?
+ ////////////////////////////////////////////////////////////////////////////////////////// 21
+ if($oggi >= 21) {
+ 
+?>
+	    <div class="col-sm-3" style="">
+	<div style=" background: url('https://via.placeholder.com/224');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/21.jpg');"class="thumb" style=""></div></div>
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/21.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+
+
+</div>
+ <div class="row">
+ 	  <?
+ ////////////////////////////////////////////////////////////////////////////////////////// 19
+ if($oggi >= 19) {
+ 
+?>
+    <div class="col-sm-3" style="">
+	<div style=" background: url('https://via.placeholder.com/224');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/19.jpg');"class="thumb" style=""></div></div>
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/19.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+ 	  <?
+ ////////////////////////////////////////////////////////////////////////////////////////// 2
+ if($oggi >= 2) {
+ 
+?>
+    <div class="col-sm-3" style="">
+	<a href="http://tepre.it/news.php?id=17"><div style=" background: url('img/2a.jpg');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/2.jpg');"class="thumb" style=""></div></div></a>
+</div>	
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/2.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+ 	  <?
+ ////////////////////////////////////////////////////////////////////////////////////////// 24
+ if($oggi >= 24) {
+ 
+?>
+	    <div class="col-sm-3" style="">
+	<div style=" background: url('https://via.placeholder.com/224');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/24.jpg');"class="thumb" style=""></div></div>
+</div>	
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/24.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+ 	  <?
+ ////////////////////////////////////////////////////////////////////////////////////////// 4
+ if($oggi >= 4) {
+ 
+?>	   
+	   <div class="col-sm-3" style="">
+	<a href="https://tepre.it/news.php?id=19"><div style=" background: url('img/4a.jpg');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/4.jpg');"class="thumb" style=""></div></div></a>
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/4.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+
+
+
+</div>
+ <div class="row">
+  	  <?
+ ////////////////////////////////////////////////////////////////////////////////////////// 6
+ if($oggi >= 6) {
+ 
+?>	
+    <div class="col-sm-3" style="">
+	<div style=" background: url('https://via.placeholder.com/224');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/6.jpg');"class="thumb" style=""></div></div>
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/6.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+
+
+  	  <?
+ ////////////////////////////////////////////////////////////////////////////////////////// 18
+ if($oggi >= 18) {
+ 
+?>	
+    <div class="col-sm-3" style="">
+	<div style=" background: url('https://via.placeholder.com/224');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/18.jpg');"class="thumb" style=""></div></div>
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/18.jpg' />
+            </div>	 
+<?	 
+ }
+?>	
+  	  <?
+ ////////////////////////////////////////////////////////////////////////////////////////// 13
+ if($oggi >= 13) {
+ 
+?>	
+	    <div class="col-sm-3" style="">
+	<div style=" background: url('https://via.placeholder.com/224');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/13.jpg');"class="thumb" style=""></div></div>
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/13.jpg' />
+            </div>	 
+<?	 
+ }
+?>	
+  	  <?
+ ////////////////////////////////////////////////////////////////////////////////////////// 5
+ if($oggi >= 5) {
+ 
+?>
+	    <div class="col-sm-3" style="">
+	<a href="http://tepre.it/news.php?id=20"><div style=" background: url('img/5a.jpg');" class="perspective" onclick="openDoor(this)"><div style="background: url('img/5.jpg');"class="thumb" style=""></div></div></a>
+</div>
+<?
+ }else{
+?>
+  <div class="col-sm-3">
+	       
+<img src='img/5.jpg' />
+            </div>	 
+<?	 
+ }
+?>
+</div>
+
+	
+	
+</div>
+</div>
+	<footer class="container-fluid bg-3 text-center">
+<p>❤️ Happy Christmas from Italy ❤️ </p><b>
+</footer>
+
+	</div></body></html>
